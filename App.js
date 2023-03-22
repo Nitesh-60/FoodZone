c9a47e535e3c643bdab549f627da651489f2a3dd
Nitesh Pandey <91657877+Nitesh-60@users.noreply.github.com>
2023-03-22 20:59:42 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,32 @@
+const heading1 = React.createElement(
+    "h1",
+    {
+        id: "title1",
+        style : {
+            color: "red",
+        },
+    },
+    "Namaste Everyone"
+);
+const heading2 = React.createElement(
+    "h2",
+    {
+        id: "title2",
+        style : {
+            color: "blue",
+        },
+    },
+    "I am Nitesh !"
+);
+
+const container = React.createElement(
+    "div",
+    {
+        id: "container",
+    },
+    [heading1,heading2]
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(container);
