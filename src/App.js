944706f1c4a35e7f1b7496af7655bc2ef956865a
Nitesh Pandey <pandeynitesh1302@gmail.com>
2023-03-22 20:52:41 +0530

    search bar added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { useState } from "react";
 
 const title = (
     <a href="/">
@@ -101,14 +102,44 @@ const RestruantCards = ({
     );
 };
 
+function filterSearch(searchText,newRestruants){
+  const filter_data = newRestruants.filter((res)=>{
+    res.data.name.includes(searchText);
+  });
+  return filter_data;
+}
+
 const Body = ()=>{
+  const [searchText, setSearchText] = useState();
+  const [newRestruants,setNewRestruants] = useState(burgerKing)
     return (
+      <>
+        <div className="search-div">
+          <input 
+          type="text" 
+          className="srch" 
+          placeholder="Search Here" 
+          value={searchText}
+          onChange = {(e)=>{
+            setSearchText(e.target.value)
+          }}
+          ></input>
+          <button 
+          className="search-btn"
+          onClick={()=>{
+            // filter the data
+            const data = filterSearch(searchText,newRestruants);
+            // return the data
+            setNewRestruants(data);
+          }}
+          >Search</button>
+        </div>
         <div className="RestruantList">
         {burgerKing.map((restruant)=>{
           return <RestruantCards {...restruant.data} keys = {restruant.data.id}/>
         })}
         </div>
-        
+      </>
     );
 };
 
